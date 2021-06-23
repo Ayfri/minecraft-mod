6b3cb7bc5cd7b6831795a926b089efc43bb591d3
Ayfri <pierre.ayfri@gmail.com>
2021-06-23 16:23:18 +0200

    chore: Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stream-Mod
+
+A simple mod that I'm making on stream on Twitch.
---
+++ src/main/kotlin/fr/ayfri/mod/Ayfri.kt
@@ -0,0 +1,10 @@
+package fr.ayfri.mod
+
+import net.fabricmc.api.ModInitializer
+
+class Ayfri : ModInitializer {
+	override fun onInitialize() {
+		println("AyMod loaded !")
+	}
+	
+}
